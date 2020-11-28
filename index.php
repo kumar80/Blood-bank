4f54179a7dd8a5633d3062d7c96f0d072d6434dc
kumar80 <chandankumar80567@gmail.com>
2020-11-29 03:39:31 +0530

    make it better

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php include_once("index.html"); ?>
+<?php 
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: *");
+
+include_once("index.html"); ?>
