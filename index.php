a41df540b8c7c8f5262af97539c6c6a93fe4a138
kumar80 <chandankumar80567@gmail.com>
2020-11-29 14:30:39 +0530

    make it better

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: *");
-include_once("login.html"); ?>
+include_once("index.html"); ?>
